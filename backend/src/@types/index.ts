a9d32925bea3f1836fe12fba56ce6ed3bd16e17e
Trần Đức Cường <clonebmn2itt@gmail.com>
2021-12-12 10:11:15 +0700

    feat: switch prisma to mongoose

---
+++ backend/src/@types/index.ts
@@ -0,0 +1,34 @@
+import type { Model, Document } from 'mongoose'
+// import { Request, Response, NextFunction } from 'express'
+export interface User {
+  name: string
+  email: string
+  password: string
+  avatarURL?: string
+}
+
+export interface IUserModel extends Model<User> {}
+export interface IUserDocument extends User, Document {
+  hashPassword: () => Promise<void>
+  checkPassword: (password: string) => boolean
+}
+
+// export interface IController<BodyType = {}, ResponseType = {}, QueryType = {}, ParamType = {}> {
+//   (
+//     req: Request<ParamType, ResponseType, BodyType, QueryType>,
+//     res: Response<ResponseType>,
+//     next: NextFunction
+//   ): void | Promise<void>
+// }
+// export interface IControllerAsync<
+//   BodyType = {},
+//   ResponseType = {},
+//   QueryType = {},
+//   ParamType = {}
+// > {
+//   (
+//     req: Request<ParamType, ResponseType, BodyType, QueryType>,
+//     res: Response<ResponseType>,
+//     next: NextFunction
+//   ): Promise<void>
+// }
---
+++ backend/src/models/user.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+import { IUserDocument } from '../@types'
+
+const userSchema = new mongoose.Schema<IUserDocument>({
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: false,
+    default: 'John',
+  },
+  avatarURL: {
+    type: String,
+    required: false,
+  },
+})
+
+userSchema.methods.hashPassword = function () {
+  const salt = bcrypt.genSaltSync(10)
+  const hash = bcrypt.hashSync(this.password, salt)
+  this.password = hash
+}
+
+userSchema.methods.checkPassword = function (password: string) {
+  return bcrypt.compareSync(password, this.password)
+}
+export const UserModel = mongoose.model('user', userSchema)
