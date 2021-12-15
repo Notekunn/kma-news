7325c8dad0fac01df1e58d1651ce7d02a9cfd249
Trần Đức Cường <clonebmn2itt@gmail.com>
2021-12-15 08:12:17 +0700

    feat(frontend): drop menu item

---
+++ frontend/src/components/Header/DropMenuItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export interface DropMenuSubItemProps {}
+
+export const DropMenuSubItem: React.FC<DropMenuSubItemProps> = (props) => {
+  return <li className="menu-sub-item">{props.children}</li>
+}
+
+export interface DropMenuItemProps {
+  title: string
+  subItems?: Omit<DropMenuItemProps, 'subItems'>[]
+}
+
+export const DropMenuItem: React.FC<DropMenuItemProps> = React.memo(({ title, subItems }) => {
+  if (!subItems || subItems.length === 0) subItems = [{ title: '' }]
+  return (
+    <li className="drop-menu-item">
+      <div className="menu-item-title">{title}</div>
+      <ul className="drop-menu-sub-list">
+        {subItems.map((e) => (
+          <DropMenuSubItem>{e.title}</DropMenuSubItem>
+        ))}
+      </ul>
+    </li>
+  )
+})
