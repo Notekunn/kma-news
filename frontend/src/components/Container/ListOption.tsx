b635a85fbb0efacec794df39dd2c6562867bebf7
lamsonkma <lamsonttbn@gmail.com>
2021-12-15 18:09:54 +0700

    update

---
+++ frontend/src/components/Container/ListOption.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export interface ItemOptionSelect { }
+
+const ListOption: React.FC<ItemOptionSelect> = ({ }) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ListOption
