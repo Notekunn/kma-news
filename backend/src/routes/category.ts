bc075f1de4aa1ec72cfeb1e3732ef05e853c5ac0
Trần Đức Cường <clonebmn2itt@gmail.com>
2021-12-18 17:24:22 +0700

    chore: add auth and guard

--- backend/src/routes/category.ts
+++ backend/src/routes/category.ts
@@ -1,9 +1,11 @@
+import { authMiddleware } from '@/middlewares/auth-middleware'
+import { guardMiddleware } from '@/middlewares/guard-middleware'
 import { Router } from 'express'
 import * as categoryController from '../controllers/category'
 const router = Router()
 
 router.get('/', categoryController.getAll)
 
-router.post('/', categoryController.create)
+router.post('/', authMiddleware, guardMiddleware('admin'), categoryController.create)
 
 export default router
