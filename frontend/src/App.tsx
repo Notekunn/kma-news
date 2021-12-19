a799b28487d760fe79e01b5034fbbe5745e0d6d4
líp đỗ đắc <daclip26@gmail.com>
2021-12-19 13:51:51 +0700

    Modifi header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header/index'
 import Footer from './components/Footer/index'
 import Container from './components/Container/index'
 import { Route, Routes } from 'react-router-dom'
+import PageRender from './components/Page_Render/index'
 import './App.css'
 import './gird.css'
 import './index.css'
@@ -13,10 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Container />} />
         <Route path="/tin-moi.epi" element={<Container />} />
+        <Route path="/page" element={<PageRender />} />
       </Routes>
       <Footer />
     </div>
   )
 }
-
 export default App
---
+++ frontend/src/components/Page_Render/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+interface Props {}
+const PageRender: React.FC<Props> = (props) => {
+  return (
+    <div className="container">
+      <h1>sadasd</h1>
+    </div>
+  )
+}
+export default PageRender
