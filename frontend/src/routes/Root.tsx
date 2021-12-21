f5ca60c981e41e263fb900ee9782be8e33672ba9
lamsonkma <lamsonttbn@gmail.com>
2021-12-21 12:57:47 +0700

    Reading Page

--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import Topic from '@/features/Topic/pages/Topic'
 import { Route, Routes } from 'react-router-dom'
-import Home from '@/features/HomePage/pages/HomePage'
 import { BasicLayout } from '@/layouts/BasicLayout'
+import Home from '@/features/HomePage/pages/HomePage'
+import Topic from '@/features/Topic/pages/Topic'
+import ReadingPage from '@/features/New/pages/ReadingPage'
 export const RootRouter = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ export const RootRouter = () => {
         <Route path="/" element={<BasicLayout />}>
           <Route path="/tin-moi.epi" element={<Home />} />
           <Route path="/chu-de" element={<Topic />} />
+          <Route path="bai-bao/:slug" element={<ReadingPage />} />
           <Route path="/" element={<Home />} />
         </Route>
         <Route path="/admin" element={<BasicLayout />}></Route>
