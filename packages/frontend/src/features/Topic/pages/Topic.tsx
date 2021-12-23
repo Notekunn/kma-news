6b61ac1c6df5022b408079c9859be3728712991f
lamsonkma <lamsonttbn@gmail.com>
2021-12-23 19:10:35 +0700

    update

--- packages/frontend/src/features/Topic/pages/Topic.tsx
+++ packages/frontend/src/features/Topic/pages/Topic.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { AiOutlineArrowsAlt } from 'react-icons/ai'
+import { AiOutlineArrowsAlt, AiOutlineStar } from 'react-icons/ai'
 import { PopUp } from '../components/PopUp'
 const Topic = () => {
   const [showPopUp, setShowPopUp] = useState(false)
@@ -15,13 +15,191 @@ const Topic = () => {
             <div className="list-topic">
               <div className="item-topic">
                 <div className="header-item-topic">
-                  <Link to="/">Năng lượng tích cực</Link>
-                  <div onClick={() => setShowPopUp(true)}>
-                    <AiOutlineArrowsAlt />
+                  <div>
+                    <Link to="/">Năng lượng tích cực</Link>
+                    <span>
+                      <AiOutlineStar />
+                    </span>
+                  </div>
+                  <div className="icon-pop-up" onClick={() => setShowPopUp(true)}>
+                    <AiOutlineArrowsAlt size="16px" />
                   </div>
                 </div>
                 <div className="list-news-topic">
                   <div className="list-news">
+                    <div className="col-4 item-news">
+                      <div className="img-item-news">
+                        <Link to="/">
+                          <img
+                            src="https://photo-baomoi.zadn.vn/w300_r3x2/2021_12_20_65_41251353/7bf856c5e5870cd95596.jpg"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+
+                      <div className="description-item-news">
+                        <Link to="/">
+                          <span>Cô giáo của những đứa trẻ khiếm thính</span>
+                        </Link>
+                        <div className="news-source">
+                          <Link to="/">
+                            <img
+                              className="logo-source"
+                              src="https://photo-baomoi.zadn.vn/a1493a27e7640e3a5775.png"
+                              alt=""
+                            />
+                          </Link>
+                          <span className="news-time">5 giờ</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-4 item-news">
+                      <div className="img-item-news">
+                        <Link to="/">
+                          <img
+                            src="https://photo-baomoi.zadn.vn/w300_r3x2/2021_12_20_65_41251353/7bf856c5e5870cd95596.jpg"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+
+                      <div className="description-item-news">
+                        <Link to="/">
+                          <span>Cô giáo của những đứa trẻ khiếm thính</span>
+                        </Link>
+                        <div className="news-source">
+                          <Link to="/">
+                            <img
+                              className="logo-source"
+                              src="https://photo-baomoi.zadn.vn/a1493a27e7640e3a5775.png"
+                              alt=""
+                            />
+                          </Link>
+                          <span className="news-time">5 giờ</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-4 item-news">
+                      <div className="img-item-news">
+                        <Link to="/">
+                          <img
+                            src="https://photo-baomoi.zadn.vn/w300_r3x2/2021_12_20_65_41251353/7bf856c5e5870cd95596.jpg"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+
+                      <div className="description-item-news">
+                        <Link to="/">
+                          <span>Cô giáo của những đứa trẻ khiếm thính</span>
+                        </Link>
+                        <div className="news-source">
+                          <Link to="/">
+                            <img
+                              className="logo-source"
+                              src="https://photo-baomoi.zadn.vn/a1493a27e7640e3a5775.png"
+                              alt=""
+                            />
+                          </Link>
+                          <span className="news-time">5 giờ</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="item-topic">
+                <div className="header-item-topic">
+                  <div>
+                    <Link to="/">Năng lượng tích cực</Link>
+                    <span>
+                      <AiOutlineStar />
+                    </span>
+                  </div>
+                  <div className="icon-pop-up" onClick={() => setShowPopUp(true)}>
+                    <AiOutlineArrowsAlt size="16px" />
+                  </div>
+                </div>
+                <div className="list-news-topic">
+                  <div className="list-news">
+                    <div className="col-4 item-news">
+                      <div className="img-item-news">
+                        <Link to="/">
+                          <img
+                            src="https://photo-baomoi.zadn.vn/w300_r3x2/2021_12_20_65_41251353/7bf856c5e5870cd95596.jpg"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+
+                      <div className="description-item-news">
+                        <Link to="/">
+                          <span>Cô giáo của những đứa trẻ khiếm thính</span>
+                        </Link>
+                        <div className="news-source">
+                          <Link to="/">
+                            <img
+                              className="logo-source"
+                              src="https://photo-baomoi.zadn.vn/a1493a27e7640e3a5775.png"
+                              alt=""
+                            />
+                          </Link>
+                          <span className="news-time">5 giờ</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-4 item-news">
+                      <div className="img-item-news">
+                        <Link to="/">
+                          <img
+                            src="https://photo-baomoi.zadn.vn/w300_r3x2/2021_12_20_65_41251353/7bf856c5e5870cd95596.jpg"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+
+                      <div className="description-item-news">
+                        <Link to="/">
+                          <span>Cô giáo của những đứa trẻ khiếm thính</span>
+                        </Link>
+                        <div className="news-source">
+                          <Link to="/">
+                            <img
+                              className="logo-source"
+                              src="https://photo-baomoi.zadn.vn/a1493a27e7640e3a5775.png"
+                              alt=""
+                            />
+                          </Link>
+                          <span className="news-time">5 giờ</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-4 item-news">
+                      <div className="img-item-news">
+                        <Link to="/">
+                          <img
+                            src="https://photo-baomoi.zadn.vn/w300_r3x2/2021_12_20_65_41251353/7bf856c5e5870cd95596.jpg"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+
+                      <div className="description-item-news">
+                        <Link to="/">
+                          <span>Cô giáo của những đứa trẻ khiếm thính</span>
+                        </Link>
+                        <div className="news-source">
+                          <Link to="/">
+                            <img
+                              className="logo-source"
+                              src="https://photo-baomoi.zadn.vn/a1493a27e7640e3a5775.png"
+                              alt=""
+                            />
+                          </Link>
+                          <span className="news-time">5 giờ</span>
+                        </div>
+                      </div>
+                    </div>
                     <div className="col-4 item-news">
                       <div className="img-item-news">
                         <Link to="/">
