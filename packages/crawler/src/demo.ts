ecc52f1bf3fade327dae625c4e86d33a26915954
Nguyễn Lam Sơn <67233512+lamsonkma@users.noreply.github.com>
2021-12-25 09:47:04 +0700

    Update services (#30)
    * update
    
    * update gitignore
    
    Co-authored-by: Trần Đức Cường <clonebmn2itt@gmail.com>

--- packages/crawler/src/demo.ts
+++ packages/crawler/src/demo.ts
@@ -1,6 +1,10 @@
 import VNExpress from './services/vnexpress'
+import BaoChinhPhu from './services/baochinhphu'
+import VietNamNet from './services/vietnamnet'
 import mongoose from 'mongoose'
+const baochinhphu = new BaoChinhPhu()
 const vnexpress = new VNExpress()
+const vietnamnet = new VietNamNet()
 async function connectDatabase() {
   await mongoose.connect(
     process.env.DATABASE_URL ||
@@ -15,9 +19,19 @@ connectDatabase().catch((e) => {
 })
 
 function main() {
-  vnexpress
+  // vnexpress
+  //   .getNewDetail(
+  //     'https://vnexpress.net/chinh-phu-yeu-cau-khan-truong-mo-duong-bay-quoc-te-4406799.html'
+  //   )
+  //   .then(console.log)
+  // baochinhphu
+  //   .getNewDetail(
+  //     'http://baochinhphu.vn/xa-hoi/khong-phai-cach-ly-y-te-voi-nguoi-nhap-canh-lam-viec-duoi-14-ngay/457101.vgp'
+  //   )
+  //   .then(console.log)
+  vietnamnet
     .getNewDetail(
-      'https://vnexpress.net/chinh-phu-yeu-cau-khan-truong-mo-duong-bay-quoc-te-4406799.html'
+      'https://vietnamnet.vn/vn/phap-luat/ho-so-vu-an/pha-an-hang-nghin-bao-thuoc-la-lau-duoi-vo-boc-phong-chong-dich-covid-19-803982.html'
     )
     .then(console.log)
 }
---
+++ packages/crawler/src/services/vietnamnet.ts
@@ -0,0 +1,81 @@
+import BaseService from './base'
+import type { CheerioAPI } from 'cheerio'
+import RssParser from 'rss-parser'
+import { IParagraph, IParagraphImage, IPost } from 'shared-types'
+import { text } from 'cheerio/lib/api/manipulation'
+
+export const RSS_URL = 'http://vietnamnet.vn/rss/'
+
+const parser = new RssParser({})
+
+export default class VietNamNet extends BaseService {
+  constructor() {
+    super()
+  }
+  async getLastedNews() {
+    const feed = await parser.parseURL(RSS_URL)
+    return feed.items.map((e) => {
+      return {
+        title: e.title || '',
+        link: e.link || '',
+        contentSnippet: e.contentSnippet || '',
+        pubDate: e.isoDate || e.pubDate || '',
+      }
+    })
+  }
+  async getNewDetail(url: string) {
+    const { data: $ } = await this.api.get<CheerioAPI>(url)
+    const title = $('.ArticleDetail > h1').text()
+    const description = $('#ArticleContent > .ArticleLead > p').text()
+    const paragraphs: IParagraph[] = []
+    const content = $('#ArticleContent').children()
+    const publishedTime = this.formatTime($('.ArticleDate').text())
+    for (const el of content) {
+      if (el.tagName == 'p') {
+        paragraphs.push({
+          type: 'text',
+          content: this.formatText($(el).text()),
+        })
+      }
+      if (el.tagName == 'table') {
+        const elem = $(el)
+        const imageUrl = elem.find('img').attr('src')
+        const imageDescription = elem.find('.image_desc').text() || ''
+        if (imageUrl) {
+          paragraphs.push({
+            type: 'image',
+            imageUrl: [imageUrl],
+            description: imageDescription,
+          })
+        }
+      }
+    }
+    const thumbnailUrl =
+      (<IParagraphImage>paragraphs.find((e) => e.type === 'image'))?.imageUrl?.[0] || ''
+    const result: IPost = {
+      title: this.formatText(title),
+      slug: BaseService.stringToSlug(title),
+      thumbnailUrl,
+      categories: [],
+      description,
+      status: 'publish',
+      source: 'vietnamnet.vn',
+      owner: 'Lam Sơn',
+      publishedAt: publishedTime,
+      paragraphs,
+    }
+    const last = paragraphs.splice(-1)
+    if (last.length != 1) return result
+    if (last[0].type === 'text') {
+      result.owner = last[0].content
+      result.paragraphs = paragraphs
+    } else {
+      result.paragraphs = [...paragraphs, ...last]
+    }
+    const category = $('.top-cate-head-title').find('a')
+    const categories = [...category].map((e) => this.formatText($(e).text()))
+    const categoriesId = (await Promise.all(categories.map(this.getCategoryId))).filter((e) => !!e)
+    result.categories = categoriesId
+    return result
+  }
+}
