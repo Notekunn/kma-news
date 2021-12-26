f0bdd678e5fc3ff93059eab0850ed1a0154967bf
clonebmn2itt@gmail.com <clonebmn2itt@gmail.com>
2021-12-27 00:06:11 +0700

    update  certbot

--- deploy/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Starting the application"
-certbot certonly --webroot -d kma-news.tech --email info@kma-news.tech -w /var/www/_letsencrypt -n --agree-tos --force-renewal
-certbot certonly --webroot -d api.kma-news.tech --email info@api.kma-news.tech -w /var/www/_letsencrypt -n --agree-tos --force-renewal
-certbot certonly --webroot -d admin.kma-news.tech --email info@admin.kma-news.tech -w /var/www/_letsencrypt -n --agree-tos --force-renewal
