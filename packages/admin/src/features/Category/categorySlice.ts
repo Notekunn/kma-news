056155f8a8b6eaff3982f1266eaf429bb4f096e2
Trần Đức Cường <clonebmn2itt@gmail.com>
2021-12-30 12:19:56 +0700

    update category slice

--- packages/admin/src/features/Category/categorySlice.ts
+++ packages/admin/src/features/Category/categorySlice.ts
@@ -1,11 +1,30 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { LoadingState } from 'shared-types'
-import { Types, getAllCategories } from 'shared-api'
+import { Types, getAllCategories, createCategory, updateCategory, deleteCategory } from 'shared-api'
 import { RootState } from '@/app/store'
 
 export const getAllAction = createAsyncThunk('category/getAll', (_, thunkAPI) => {
   return getAllCategories({})
 })
+
+export const createAction = createAsyncThunk(
+  'category/create',
+  (_: Types.APIParameter.CreateCategory, thunkAPI) => {
+    return createCategory(_)
+  }
+)
+
+export const updateAction = createAsyncThunk(
+  'category/update',
+  (_: Types.APIParameter.UpdateCategory, thunkAPI) => {
+    return updateCategory(_)
+  }
+)
+
+export const deleteAction = createAsyncThunk('category/delete', (_: string, thunkAPI) => {
+  return deleteCategory(_)
+})
+
 interface CategoryState {
   data: Types.APIResponse.GetAllCategories
   loading: LoadingState
@@ -31,6 +50,45 @@ const categorySlice = createSlice({
         state.data = action.payload
       })
       .addCase(getAllAction.rejected, (state, action) => {
+        state.loading = 'error'
+        state.message = action.error.message
+      })
+    builder
+      .addCase(createAction.pending, (state) => {
+        state.loading = 'pending'
+      })
+      .addCase(createAction.fulfilled, (state, action) => {
+        state.loading = 'done'
+        state.data.push(action.payload)
+      })
+      .addCase(createAction.rejected, (state, action) => {
+        state.loading = 'error'
+        state.message = action.error.message
+      })
+    builder
+      .addCase(updateAction.pending, (state) => {
+        state.loading = 'pending'
+      })
+      .addCase(updateAction.fulfilled, (state, action) => {
+        state.loading = 'done'
+        state.data = state.data.map((e) => {
+          if (e._id !== action.payload._id) return e
+          return action.payload
+        })
+      })
+      .addCase(updateAction.rejected, (state, action) => {
+        state.loading = 'error'
+        state.message = action.error.message
+      })
+    builder
+      .addCase(deleteAction.pending, (state) => {
+        state.loading = 'pending'
+      })
+      .addCase(deleteAction.fulfilled, (state, action) => {
+        state.loading = 'done'
+        state.data = state.data.filter((e) => e._id !== action.payload._id)
+      })
+      .addCase(deleteAction.rejected, (state, action) => {
         state.loading = 'error'
         state.message = action.error.message
       })
