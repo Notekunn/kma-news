d1cc3c3aaf1312aab00ee2603b91746b944babaf
líp đỗ đắc <daclip26@gmail.com>
2021-12-31 10:36:08 +0700

    api_paramater

--- packages/shared-api/src/types/api_parameter.ts
+++ packages/shared-api/src/types/api_parameter.ts
@@ -41,4 +41,25 @@ export namespace APIParameter {
     id: string
     slug: string
   }
+
+  export interface GetAllPosts extends Pagination {}
+
+  export interface UpdatePost extends Partial<IPost> {
+    _id: string
+  }
+
+  export interface CreatePost extends Partial<IPost> {}
+
+  export interface CreateCategory {
+    title: ICategory['title']
+    description?: ICategory['description']
+    parrentId?: string | null
+  }
+
+  export interface UpdateCategory extends Partial<CreateCategory> {
+    _id: string
+    slug?: ICategory['slug']
+  }
+
+  export interface GetAllCategories extends Pagination {}
 }
