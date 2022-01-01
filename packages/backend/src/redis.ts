098fb95a01516de23860611031c37a755cab4359
Trần Đức Cường <clonebmn2itt@gmail.com>
2022-01-01 09:11:53 +0700

    fix cors

--- packages/backend/src/redis.ts
+++ packages/backend/src/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis'
 
 const client = createClient({
-  url: process.env.URL,
+  url: process.env.REDIS_URL || 'redis://localhost:6379',
 })
 
 export default client
