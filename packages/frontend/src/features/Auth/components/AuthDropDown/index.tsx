42e6ea1150b5474061de1158786fdaf04fa2460c
líp đỗ đắc <daclip26@gmail.com>
2022-01-01 23:22:58 +0700

    format Css

--- packages/frontend/src/features/Auth/components/AuthDropDown/index.tsx
+++ packages/frontend/src/features/Auth/components/AuthDropDown/index.tsx
@@ -16,13 +16,13 @@ export const AuthDropDown: React.FC<AuthDropDownProps> = (props) => {
   return (
     <div className={visible ? 'auth-drop' : 'auth-drop--hide'}>
       <ul className="auth-drop__list">
-        <Link to="/de-xuat" className="auth-drop__item">
+        <Link to="/ca-nhan/de-xuat" className="auth-drop__item">
           <GiRibbonMedal className="auth-drop__icon" />
           <li>
             <div className="auth-drop__item--a">Tin đề xuất</div>
           </li>
         </Link>
-        <Link to="/doc-gan-day" className="auth-drop__item">
+        <Link to="/ca-nhan/doc-gan-day" className="auth-drop__item">
           <li>
             <div className="auth-drop__item--a">
               <GiBackwardTime className="auth-drop__icon" />
@@ -30,7 +30,7 @@ export const AuthDropDown: React.FC<AuthDropDownProps> = (props) => {
             </div>
           </li>
         </Link>
-        <Link to="/tin-da-luu" className="auth-drop__item">
+        <Link to="/ca-nhan/tin-da-luu" className="auth-drop__item">
           <li>
             <div className="auth-drop__item--a">
               <BsSave2 className="auth-drop__icon" />
