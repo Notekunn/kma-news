22b640aaa7bd24c1b290d50548b9517488b8b997
Trần Đức Cường <36887165+Notekunn@users.noreply.github.com>
2022-01-13 14:38:37 +0700

    Fix publisher on render page (#63)
    * Update ci.yml
    
    * fix render page

--- packages/shared-api/src/types/api_response.ts
+++ packages/shared-api/src/types/api_response.ts
@@ -1,7 +1,6 @@
-import { IUser, ObjectWithID, IPost, ICategory } from 'shared-types'
-
-type UserWithoutPassword = Exclude<IUser, 'password'>
-export namespace APIResponse {
+import { IUser, ObjectWithID, IPost, ICategory, IPublisherDocument, IPublisher } from 'shared-types'
+declare type UserWithoutPassword = Exclude<IUser, 'password'>
+export declare namespace APIResponse {
   export type GetMany<T> = ObjectWithID<T>[]
   export interface Login {
     access_token: string
@@ -23,19 +22,15 @@ export namespace APIResponse {
     publishAt: Date
     [paragraphs: number]: paragraph
   }
-
   export interface RefreshToken {
     access_token: string
     tokenExpiration: string
     user: ObjectWithID<UserWithoutPassword>
   }
-
   export interface Logout {
     message: string
   }
-
   export interface Profile extends ObjectWithID<UserWithoutPassword> {}
-
   interface LocationReport {
     name: string
     death: number
@@ -54,36 +49,32 @@ export namespace APIResponse {
     | 'recoveredToday'
     | 'treatingToday'
   type LocationData = Record<LocationField, number>
-  // Data raw trả về
   export type Covid19Raw = Record<'total' | 'today', Record<'internal' | 'world', LocationReport>> &
     Record<'locations', LocationReport[]>
-  // Data sau khi xử lý
   export type Covid19 = Record<'internal' | 'world', LocationData> &
     Record<'locations', Array<LocationData & Record<'name', string>>>
-
   export type GetAllUsers = GetMany<UserWithoutPassword>
-
   export interface CreateUser extends ObjectWithID<UserWithoutPassword> {}
-
   export interface UpdateUser extends ObjectWithID<UserWithoutPassword> {}
-
   export interface DeleteUser extends ObjectWithID<UserWithoutPassword> {}
-
   export type GetAllPosts = GetMany<
-    Pick<
-      IPost,
-      'title' | 'slug' | 'description' | 'categories' | 'source' | 'thumbnailUrl' | 'publishedAt'
-    >
+    Pick<IPost, 'title' | 'slug' | 'description' | 'publisher' | 'thumbnailUrl' | 'publishedAt'> & {
+      url: string
+    }
   >
-
-  export interface GetOnePost extends ObjectWithID<IPost> {}
-
-  export interface UpdatePost extends ObjectWithID<IPost> {}
-
-  export interface CreatePost extends ObjectWithID<IPost> {}
-
-  export interface DeletePost extends ObjectWithID<IPost> {}
-
+  export type GetOnePost = ObjectWithID<Omit<IPost, 'publisher'>> & {
+    url: string
+    publisher: IPublisher
+  }
+  export interface UpdatePost extends ObjectWithID<IPost> {
+    url: string
+  }
+  export interface CreatePost extends ObjectWithID<IPost> {
+    url: string
+  }
+  export interface DeletePost extends ObjectWithID<IPost> {
+    url: string
+  }
   export interface CreateCategory extends ObjectWithID<ICategory> {}
 
   export interface UpdateCategory extends ObjectWithID<ICategory> {}
@@ -99,4 +90,6 @@ export namespace APIResponse {
     subItems: TreeCategory[]
   }
   export type GetTreeCategories = TreeCategory[]
+  export {}
 }
+export {}
