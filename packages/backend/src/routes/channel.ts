b043bb7069720332871b38a2bf5cd4ee5b320f7a
Trần Đức Cường <clonebmn2itt@gmail.com>
2022-01-15 17:27:03 +0700

    show info channel

--- packages/backend/src/routes/channel.ts
+++ packages/backend/src/routes/channel.ts
@@ -2,6 +2,7 @@ import getAllChannels from '@/controllers/channel/getAll'
 import createChannel from '@/controllers/channel/create'
 import updateChannel from '@/controllers/channel/update'
 import deleteChannel from '@/controllers/channel/remove'
+import getOneChannel from '@/controllers/channel/getOne'
 import { authMiddleware } from '@/middlewares/auth-middleware'
 import { Router } from 'express'
 
@@ -9,6 +10,7 @@ const router = Router()
 router
   .get('/', authMiddleware, getAllChannels)
   .post('/', authMiddleware, createChannel)
+  .get('/:id', authMiddleware, getOneChannel)
   .patch('/:id', authMiddleware, updateChannel)
   .delete('/:id', authMiddleware, deleteChannel)
 export default router
