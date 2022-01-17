04b9602f1576f599f9b2cce989f4172170400b14
Trần Đức Cường <clonebmn2itt@gmail.com>
2022-01-17 13:15:05 +0700

    fix env load

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,4 +1,8 @@
 import express from 'express'
+import dotenv from 'dotenv'
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 import mongoose from 'mongoose'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
@@ -6,12 +10,8 @@ import cors from 'cors'
 import routes from './routes'
 import errorHandler from './middlewares/error-handler'
 import client from './redis'
-import dotenv from 'dotenv'
-import { load } from 'env-defaults'
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config()
-}
 const app = express()
+import { load } from 'env-defaults'
 
 const { PORT, BACKEND_HOST, FRONTEND_HOST, ADMIN_HOST, DATABASE_URL } = load({
   PORT: 8888,
