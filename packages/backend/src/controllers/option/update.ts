502e774b840bfed2125c1e935f5ef4d23bf566b3
Trần Đức Cường <clonebmn2itt@gmail.com>
2022-01-17 10:26:55 +0700

    add upsert when update option

--- packages/backend/src/controllers/option/update.ts
+++ packages/backend/src/controllers/option/update.ts
@@ -7,7 +7,7 @@ const updateOption = errorWrapper(async (req, res, next) => {
   const { name } = req.params
   const { error, value } = updateValidator.validate(req.body)
   if (error) throw error
-  const option = await OptionModel.findOneAndUpdate({ name }, value, { new: true })
+  const option = await OptionModel.findOneAndUpdate({ name }, value, { new: true, upsert: true })
   if (!option) throw new NotFoundExeption('Option not found')
   res.send(option)
 })
