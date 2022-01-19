04d65f60404eb34e66df8ca506ab070541b5aa50
Trần Đức Cường <clonebmn2itt@gmail.com>
2022-01-19 11:54:19 +0700

    Add security layout and refactor route

--- packages/frontend/src/routes/Root.tsx
+++ packages/frontend/src/routes/Root.tsx
@@ -6,6 +6,7 @@ import Topic from '@/features/Topic/pages/Topic'
 import ReadingPage from '@/features/Post/pages/ReadingPage'
 import { HotTopicPage } from '@/features/Post/pages/HotTopicPage'
 import { BlankLayout } from '@/layouts/BlankLayout'
+import { SecurityLayout } from '@/layouts/SecurityLayout'
 const PersonalPage = React.lazy(() => import('@/features/Personal/pages/PersonalPage'))
 const HistoryPage = React.lazy(() => import('@/features/Personal/pages/HistoryPage'))
 const SuggestPage = React.lazy(() => import('@/features/Personal/pages/SuggestPage'))
@@ -22,17 +23,20 @@ export const RootRouter = () => {
           <Route path="/" element={<BasicLayout />}>
             <Route index element={<Home />} />
             <Route path="tin-moi.epi" element={<Home />} />
-            <Route path="chu-de" element={<Topic />} />
             <Route path="bai-bao/:slug" element={<ReadingPage />} />
             <Route path="phong-chong-dich-covid-19" element={<HotTopicPage />} />
-            <Route path="ca-nhan" element={<PersonalPage />}>
+          </Route>
+          <Route path="/ca-nhan" element={<SecurityLayout />}>
+            <Route path="" element={<PersonalPage />}>
               <Route path="de-xuat" element={<SuggestPage />} />
               <Route path="doc-gan-day" element={<HistoryPage />} />
               <Route path="muc-da-luu" element={<FavoritePage />} />
               <Route path="muc-cua-ban" element={<CategoryPage />} />
               <Route path="theo-doi" element={<SubscriptionPage />} />
-              <Route path="*" element={<></>} />
             </Route>
+          </Route>
+          <Route path="/chu-de" element={<BasicLayout />}>
+            <Route path="" element={<Topic />} />
           </Route>
           <Route path="auth/login" element={<BlankLayout />}>
             <Route path="zalo" element={<ZaloLoginPage />} />
